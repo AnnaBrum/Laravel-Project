8f4596f1bff62410acdfac75f5889cd06e6c46b8
tobias-ahlund <tobias.ahlund@gmail.com>
2023-03-15 14:53:37 +0100

    added login test

---
+++ my-movies/tests/Feature/LoginTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_view_login_form()
+    {
+        $response = $this->get("/");
+        $response->assertSeeText("Email");
+        $response->assertStatus(200);
+    }
+
+    public function test_login_user()
+    {
+        $user = new User();
+        $user->name = "Runar";
+        $user->email = "runar@yrgo.se";
+        $user->password = Hash::make("1234567");
+        $user->save();
+
+        $response = $this
+            ->followingRedirects()
+            ->post("login", [
+                "email" => "runar@yrgo.se",
+                "password" => "1234567",
+            ]);
+
+        $response->assertSeeText("Signed in user: Harald");
+    }
+
+    public function test_login_user_without_password()
+    {
+        $response = $this
+            ->followingRedirects()
+            ->post("login", [
+                "email" => "runar@yrgo.se",
+            ]);
+
+        $response->assertSeeText("The password field is required.");
+    }
+}
